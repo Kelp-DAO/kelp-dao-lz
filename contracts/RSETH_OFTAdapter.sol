db371cc9537cf70326442c1beb72dae828385ac9
Gus <gustavo@staderlabs.com>
2024-02-02 10:44:27 -0600

    create RSETH OFT contracts

---
+++ contracts/RSETH_OFTAdapter.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+import {OFTAdapter} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFTAdapter.sol";
+
+/// @dev contract used for Origin chain where the token is already deployed
+contract RSETH_OFTAdapter is OFTAdapter {
+    constructor(address _token, address _lzEndpoint, address _delegate) OFTAdapter(_token, _lzEndpoint, _delegate) {}
+}
