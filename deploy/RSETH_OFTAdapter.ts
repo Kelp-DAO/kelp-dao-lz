5ea7e7d3e67c7ba9c2bc5c8bafbf158f9e02c654
Gus <gustavo@staderlabs.com>
2024-02-02 10:44:58 -0600

    change deploy scripts for RSETH OFT contracts

---
+++ deploy/RSETH_OFTAdapter.ts
@@ -0,0 +1,58 @@
+import assert from 'assert'
+
+import { type DeployFunction } from 'hardhat-deploy/types'
+
+const contractName = 'RSETH_OFTAdapter'
+const originChainTokenAddress = '0xA1290d69c65A6Fe4DF752f95823fae25cB99e5A7' // RSETH token address on ETH mainnet
+
+const deploy: DeployFunction = async (hre) => {
+    const { getNamedAccounts, deployments } = hre
+
+    const { deploy } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    assert(deployer, 'Missing named deployer account')
+
+    console.log(`Network: ${hre.network.name}`)
+    console.log(`Deployer: ${deployer}`)
+
+    const chainId = await hre.getChainId()
+    if (chainId !== '1') {
+        console.error('This deployment script is only meant for Ethereum mainnet')
+        return
+    }
+
+    // This is an external deployment pulled in from @layerzerolabs/lz-evm-sdk-v2
+    //
+    // @layerzerolabs/toolbox-hardhat takes care of plugging in the external deployments
+    // from @layerzerolabs packages based on the configuration in your hardhat config
+    //
+    // For this to work correctly, your network config must define an eid property
+    // set to `EndpointId` as defined in @layerzerolabs/lz-definitions
+    //
+    // For example:
+    //
+    // networks: {
+    //   fuji: {
+    //     ...
+    //     eid: EndpointId.AVALANCHE_V2_TESTNET
+    //   }
+    // }
+    const endpointV2Deployment = await hre.deployments.get('EndpointV2')
+
+    const { address } = await deploy(contractName, {
+        from: deployer,
+        args: [
+            originChainTokenAddress, // symbol
+            endpointV2Deployment.address, // LayerZero's EndpointV2 address
+            deployer, // owner
+        ],
+        log: true,
+        skipIfAlreadyDeployed: false,
+    })
+
+    console.log(`Deployed contract: ${contractName}, network: ${hre.network.name}, address: ${address}`)
+}
+
+module.exports.tags = [contractName]
+export default deploy
